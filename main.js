6d80e1e3bc0ed816b8d649d1eedff746c7ac3cf5
Stanchuks <chukwustanislaus@gmail.com>
2023-03-27 19:10:40 +0430

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+var navbar= document.getElementById("navbar");
+        function showMenu(){
+            navbar.style.right = "0";
+        }
+        function hideMenu(){
+            navbar.style.right = "-200px";
+        }
+    
+
+
+
+
+
+
+
+
+
+
